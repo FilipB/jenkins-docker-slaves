eecc31aaca3e096d64460d4b3be131a7927c51c3
Filip Brychta <fbrychta@redhat.com>
2018-03-06 13:39:47 +0100

    Adding OS template

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 oc create -f jenkins-slave-ansible/buildconfig-template.yaml
 oc new-app jenkins-slave-ansible-builder
 
+oc create -f jenkins-slave-python-selenium/buildconfig-template.yaml
+oc new-app jenkins-slave-python-selenium-builder
